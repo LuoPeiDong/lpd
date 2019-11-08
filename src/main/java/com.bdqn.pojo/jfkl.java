c19151190a2aa86b5822b3350b914054dd7fc834
HTML55555 <891729443@qq.com>
2019-11-08 16:37:09 +0800

    复合卡的凯撒将符合开的

--- src/main/java/com.bdqn.pojo/jfkl.java
+++ src/main/java/com.bdqn.pojo/jfkl.java
@@ -3,4 +3,56 @@ package com.bdqn.pojo;
 public class jfkl {
     private  String name;
     private int a;
+    private String addd;
+    private String addds;
+    private String adddss;
+    private String adddsss;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public String getAddd() {
+        return addd;
+    }
+
+    public void setAddd(String addd) {
+        this.addd = addd;
+    }
+
+    public String getAddds() {
+        return addds;
+    }
+
+    public void setAddds(String addds) {
+        this.addds = addds;
+    }
+
+    public String getAdddss() {
+        return adddss;
+    }
+
+    public void setAdddss(String adddss) {
+        this.adddss = adddss;
+    }
+
+    public String getAdddsss() {
+        return adddsss;
+    }
+
+    public void setAdddsss(String adddsss) {
+        this.adddsss = adddsss;
+    }
 }
