1e5a0fe7b04b9a204260fd97fe0cae45ff11945d
HTML55555 <891729443@qq.com>
2019-11-08 16:39:57 +0800

    复合卡的凯撒将符合开的

--- src/main/java/com.bdqn.pojo/nafjdsafjkd.java
+++ src/main/java/com.bdqn.pojo/nafjdsafjkd.java
@@ -1,11 +1,10 @@
 package com.bdqn.pojo;
 
-public class jfkl {
+public class nafjdsafjkd {
     private  String name;
     private int a;
     private String addd;
     private String addds;
     private String adddss;
     private String adddsss;
-
 }
